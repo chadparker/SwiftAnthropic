59e4ad6c2fd4aa5b4350ae997f2f7eea8cc20fce
Chad Parker <cparker@rzsoftware.com>
2024-06-22 11:43:35 -0700

    add swift-log

--- Package.swift
+++ Package.swift
@@ -13,15 +13,27 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftAnthropic",
-            targets: ["SwiftAnthropic"]),
+            targets: ["SwiftAnthropic"]
+        ),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/apple/swift-log.git",
+            .upToNextMajor(from: Version(1, 0, 0))
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftAnthropic"),
+            name: "SwiftAnthropic",
+            dependencies: [
+                .product(name: "Logging", package: "swift-log"),
+            ]
+        ),
         .testTarget(
             name: "SwiftAnthropicTests",
-            dependencies: ["SwiftAnthropic"]),
+            dependencies: ["SwiftAnthropic"]
+        ),
     ]
 )
